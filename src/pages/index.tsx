c2590eb09a8110b9817f369b73420c13d6949eb5
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-04-23 15:06:03 -0300

    Format episodes response

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
-import { Header } from '../components/Header'
-import styles from '../styles/Home.module.css'
+import { GetStaticProps } from "next";
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export default function Home(props) {
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+interface File {
+  url: string;
+  type: string;
+  duration: number;
+}
+
+interface EpisodesResponse {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: File;
+}
+
+interface EspiodesFormated {
+  id: string;
+  title: string;
+  members: string;
+  publishedAt: string;
+  thumbnail: string;
+  description: string;
+  file: File;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
+
+
+type HomeProps = {
+  episodes: EspiodesFormated[]
+}
+
+export default function Home(props: HomeProps) {
   return (
    <div>
      <h1>Index</h1>
@@ -11,13 +49,33 @@ export default function Home(props) {
 }
 
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get<EpisodesResponse[]>('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
 
+
+  const episodes = data.map(item => {
+    return {
+      ...item,
+      publishedAt: format(parseISO(item.published_at), 'd MM yy', { locale: ptBR }),
+      duration: Number(item.file.duration),
+      durationAsString: convertDurationToTimeString(item.file.duration),
+      url: item.file.url
+    }
+  });
+
+  console.log(episodes)
+ 
+
+ 
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8
   }
