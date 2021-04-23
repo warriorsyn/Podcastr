4daf7fd6a1a5abd65dd09fd6ec2efcaa11fc2555
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-04-23 16:58:21 -0300

    Latest podcasts section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from "next";
+import Image from "next/image";
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import styles from './home.module.scss';
 
 interface File {
   url: string;
@@ -36,14 +38,41 @@ interface EspiodesFormated {
 
 
 type HomeProps = {
-  episodes: EspiodesFormated[]
+  latestEpisodes: EspiodesFormated[],
+  allEpisodes: EspiodesFormated[]
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-   <div>
-     <h1>Index</h1>
-    <p>{JSON.stringify(props.episodes)}</p>
+   <div className={styles.homepage}>
+     <section className={styles.latestEpisodes}> 
+      <h2>Últimos lançamentos</h2>
+
+      <ul>
+        { latestEpisodes.map(episode => (
+          <li key={episode.id}>
+
+            <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit="cover"/>
+
+            <div className={styles.episodeDetails}>
+              <a href="">{episode.title}</a>
+              <p>{episode.members}</p>
+              <span>{episode.publishedAt}</span>
+              <span>{episode.durationAsString}</span>
+            </div>
+
+            <button>
+              <img src="/play-green.svg" alt="Tocando"/>
+            </button>
+            
+          </li>
+        )) }
+      </ul>
+     </section>
+
+     <section className={styles.allEpisodes}>
+
+     </section>
    </div>
   )
 }
@@ -69,13 +98,13 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   });
 
-  console.log(episodes)
- 
-
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
  
   return {
     props: {
-      episodes
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8
   }
