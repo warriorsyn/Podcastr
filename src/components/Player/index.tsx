41d81daa648ab15868d8e79b0cd544412d83e1ac
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-04-24 18:43:28 -0300

    Play audio and control play and pause

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,13 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import Image from 'next/image';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
+
 import { playerContext } from '../../context/PlayerContext';
+
 
 import styles from './styles.module.scss';
 
 export function Player() {
-    const { episodeList, currentEpisodeIndex } = useContext(playerContext);
+    const { episodeList, currentEpisodeIndex, isPlaying, tooglePlay, setPlayingState } = useContext(playerContext);
+
+    const audioRef = useRef<HTMLAudioElement>(null);
 
     const episode = episodeList[currentEpisodeIndex];
+
+    useEffect(() => {
+        if (!audioRef.current) {
+            return;
+        }
+        isPlaying ? audioRef.current.play() : audioRef.current.pause();
+    }, [isPlaying]);
 
     return (
         <div className={styles.playerContainer}>
@@ -32,29 +45,44 @@ export function Player() {
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        
+
+                        { episode ? (
+                           <Slider 
+                            trackStyle={{ backgroundColor: '#04d361' }}
+                            railStyle={{ backgroundColor: '#9f75ff' }}
+                            handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+                           />
+                        ) : (
+                            <div className={styles.emptySlider} />
+                        ) }
+
                     </div>
                     <span>00:00</span>
                 </div>
 
+                { episode && (
+                    <audio ref={audioRef} src={episode.url} autoPlay onPlay={() => setPlayingState(true)} onPause={() => setPlayingState(false)} />
+                ) }
+
                 <div className={styles.buttons}>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar"/>
                     </button>
 
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-previous.svg" alt="Embaralhar"/>
                     </button>
 
-                    <button type="button" className={styles.playButton}>
-                        <img src="/play.svg" alt="Embaralhar"/>
+                    <button type="button" className={styles.playButton}  disabled={!episode} onClick={() => tooglePlay()} >
+                        { !isPlaying ? <img src="/play.svg" alt="Tocar"/> : <img src="/pause.svg" alt="Pausar"/> }
                     </button>
 
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar próxima"/>
                     </button>
 
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/repeat.svg" alt="Repetir"/>
                     </button>
                 </div>
--- src/context/PlayerContext.tsx
+++ src/context/PlayerContext.tsx
@@ -11,7 +11,10 @@ interface Episode {
 type PlayerContextData = {
     episodeList: Array<Episode>;
     currentEpisodeIndex: number;
+    isPlaying: boolean;
     play: (episode: any) => void;
+    tooglePlay: () => void;
+    setPlayingState: (state: boolean) => void;
 }
 
 export const playerContext = createContext({} as PlayerContextData);
@@ -20,14 +23,24 @@ export default function PlayerProvider({ children }) {
 
     const [episodeList, setEpisodeList] = useState([]);
     const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     function play(episode: any) {
         setEpisodeList([episode]);
         setCurrentEpisodeIndex(0);
+        setIsPlaying(true);
+    }
+
+    function tooglePlay() {
+        setIsPlaying(!isPlaying);
+    }
+
+    function setPlayingState(state: boolean) {
+        setIsPlaying(state)
     }
 
     return (
-        <playerContext.Provider value={{ episodeList, currentEpisodeIndex, play}}>
+        <playerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, tooglePlay, setPlayingState }}>
             {children}
         </playerContext.Provider>
     ) 
